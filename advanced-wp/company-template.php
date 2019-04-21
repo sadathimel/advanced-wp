9d27c4200055d1b78630ed2565b36cbf822e2cf4
Sadat Hossen Himel <40139304+sadathimel@users.noreply.github.com>
2019-04-22 00:55:35 +0600

    Add files via upload

---
+++ advanced-wp/company-template.php
@@ -0,0 +1,29 @@
+<?php
+/*
+   Template Name: Company Layout
+ */
+?>
+<?php get_header();?>
+    <div class="container content">
+        <div class="main block">
+			<?php if(have_posts()) : ?>
+				<?php while(have_posts()) : the_post();?>
+                    <article class="page">
+                        <h2><?php the_title();?></h2>
+                        <p class="phone">Call Us: 01825-167271</p>
+						<?php the_content();?>
+                    </article>
+				<?php endwhile; ?>
+			<?php else : ?>
+				<?php echo apautop('sorry, no posts where found'); ?>
+			<?php endif; ?>
+        </div>
+
+        <div class="side">
+		    <?php if(is_active_sidebar('sidebar')) : ?>
+			    <?php dynamic_sidebar('sidebar');?>
+		    <?php endif; ?>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
